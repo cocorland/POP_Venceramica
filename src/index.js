6ffa774fcfa70c5f3685bf18ef2372689c7b74cc
Administrador Venceramica <adminvencer@Venceramica.com>
2021-08-16 09:25:35 -0400

    Se puede: ingresar al sistema, visualizar datos en pantalla, visualizar un pdf, salir del sistema. No existe CSS

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Auth0Provider } from '@auth0/auth0-react';
 import Routes from './routes/Routes';
 import './css/estilos.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Routes />
+    <Auth0Provider 
+      domain="dev-tz3czku2.us.auth0.com" 
+      clientId="nkPU8SOAgkKwb0Q0tYQx88tyKaSs17Hg" 
+      redirectUri={window.location.origin}
+    >
+      <Routes />
+    </Auth0Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
--- src/pages/App.js
+++ src/pages/App.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import { useAuth0 } from '@auth0/auth0-react';
+import { LoginButton } from './Login';
+import { LogoutButton } from './Logout';
+import { Perfil } from './Perfil';
 
 function App() {
+    const {isAuthenticated} = useAuth0();
     return (
         <div style={{textAlign: 'center'}}>
             <br />
-           <a href="./pdf" target="_blank"> <button>Ir a PDF</button></a>
+            {isAuthenticated ? (
+                <>
+                    <Perfil/>
+                    <LogoutButton/>
+                </>
+            ) : (
+                <LoginButton/> 
+            )}
         </div>
     );
 }
